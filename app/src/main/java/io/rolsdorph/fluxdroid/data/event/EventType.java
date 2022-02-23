6a963ea1c663bec44cd822e69556dcd54dda65b2
Mads Rolsdorph <m.rolsdorph@gmail.com>
2022-02-23 08:48:23 +0100

    Remove incoming call event
    Not sure if past-Mads saw something that would make this possible? Kicking it down the road for now anyway, as it doesn't seem trivial to access

--- app/src/main/java/io/rolsdorph/fluxdroid/data/event/EventType.java
+++ app/src/main/java/io/rolsdorph/fluxdroid/data/event/EventType.java
@@ -3,10 +3,9 @@ package io.rolsdorph.fluxdroid.data.event;
 import io.rolsdorph.fluxdroid.R;
 
 public enum EventType {
-    SystemStartup(R.string.evt_system_startup),
-    IncomingCall(R.string.evt_incoming_call),
-    OutgoingCall(R.string.evt_outgoing_call),
-    IncomingText(R.string.evt_incoming_text);
+    SystemStartup(R.string.evt_system_startup, Intent.ACTION_BOOT_COMPLETED),
+    OutgoingCall(R.string.evt_outgoing_call, Intent.ACTION_NEW_OUTGOING_CALL),
+    IncomingText(R.string.evt_incoming_text, Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
 
     private final int configKey;
 
